d1cedfa00901fee84cd86f27dd6e2593a8749493
Stoltz, Jendrik <jendrik.stoltz@haw-hamburg.de>
2021-04-26 11:25:17 +0000

    Resolve "Projekt/Simulator entgültig aufsetzen"

---
+++ esep_ss2021_Gruppe2_3/test/tests.cpp
@@ -0,0 +1,14 @@
+/*
+ * tests.cc
+ *
+ *  Created on: 13.04.2021
+ *      Author: jendr
+ */
+
+#include "tests.h"
+
+TEST(DummyTest,Dummy){
+	ASSERT_EQ(1, 1);
+}
+
+
---
+++ esep_ss2021_Gruppe2_3/test/tests.h
@@ -0,0 +1,12 @@
+/*
+ * tests.h
+ *
+ *  Created on: 13.04.2021
+ *      Author: jendr
+ */
+
+#ifndef TESTS_H_
+#define TESTS_H_
+#include <gtest/gtest.h>
+
+#endif /* TESTS_H_ */
