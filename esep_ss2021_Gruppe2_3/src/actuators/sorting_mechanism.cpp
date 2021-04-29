182ff88d3570ac20a7f88f85250922002039315a
Justin <justinhoffmann2000@googlemail.com>
2021-04-29 14:31:56 +0200

    nochmal

---
+++ esep_ss2021_Gruppe2_3/src/actuators/sorting_mechanism.cpp
@@ -0,0 +1,18 @@
+/*
+ * sorting_mechanism.cpp
+ *
+ *  Created on: 29.04.2021
+ *      Author: justi
+ */
+#include "sorting_mechanism.h"
+
+namespace hal {
+
+SortingMechanism::SortingMechanism(std::shared_ptr<GPIOWrapper> gpio) {
+	_gpio = gpio;
+}
+
+}
+
+
+
