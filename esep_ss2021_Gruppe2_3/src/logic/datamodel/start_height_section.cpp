c47ca059dcc6a4d4eddb66b0183178d60ea77711
Justin Hoffmann <justin.hoffmann@haw-hamburg.de>
2021-05-21 19:58:32 +0200

    Add start_height_section skeleton

---
+++ esep_ss2021_Gruppe2_3/src/logic/datamodel/start_height_section.cpp
@@ -0,0 +1,21 @@
+/*
+ * start_height_section.cpp
+ *
+ *  Created on: 21.05.2021
+ *      Author: justi
+ */
+
+#include <logic/datamodel/start_height_section.h>
+
+namespace datamodel {
+
+StartHeightSection::StartHeightSection() {
+    // TODO Auto-generated constructor stub
+
+}
+
+StartHeightSection::~StartHeightSection() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace datamodel */
---
+++ esep_ss2021_Gruppe2_3/src/logic/datamodel/start_height_section.h
@@ -0,0 +1,21 @@
+/*
+ * start_height_section.h
+ *
+ *  Created on: 21.05.2021
+ *      Author: justi
+ */
+
+#ifndef SRC_LOGIC_DATAMODEL_START_HEIGHT_SECTION_H_
+#define SRC_LOGIC_DATAMODEL_START_HEIGHT_SECTION_H_
+
+namespace datamodel {
+
+class StartHeightSection {
+public:
+    StartHeightSection();
+    virtual ~StartHeightSection();
+};
+
+} /* namespace datamodel */
+
+#endif /* SRC_LOGIC_DATAMODEL_START_HEIGHT_SECTION_H_ */
