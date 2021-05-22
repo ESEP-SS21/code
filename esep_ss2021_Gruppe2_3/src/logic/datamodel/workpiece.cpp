fd36b3a667c3ab84c73cee422626108fd77e61bc
Justin Hoffmann <justin.hoffmann@haw-hamburg.de>
2021-05-22 18:50:15 +0200

    Refactor _last_id incrementation

--- esep_ss2021_Gruppe2_3/src/logic/datamodel/workpiece.cpp
+++ esep_ss2021_Gruppe2_3/src/logic/datamodel/workpiece.cpp
@@ -14,11 +14,8 @@ namespace datamodel {
 id Workpiece::_last_id = 0;
 
 Workpiece::Workpiece() : wprc_id(_last_id) {
-    if(_last_id >= 0x1fffff) {
-        _last_id = 0;
-    } else {
-        _last_id++;
-    }
+    static constexpr int max_id_in_bits = 21;
+    _last_id = (_last_id + 1) % ((1 << max_id_in_bits) + 1);
 }
 
 Workpiece::Workpiece(EncodedWorkpiece& encoded_wrpc, int encoded_id) : wprc_id(encoded_id) {
@@ -32,11 +29,7 @@ std::shared_ptr<EncodedWorkpiece> Workpiece::encode() const {
 
 void Workpiece::determine_workpiece_type() {
     if(HAS_BOHRUNG(height_1)) {
-        if(this->is_metallic) {
-            this->type = WorkpieceType::WRPC_HM;
-        } else {
-            this->type = WorkpieceType::WRPC_HB;
-        }
+        this-> type = is_metallic ? WorkpieceType::WRPC_HM : WorkpieceType::WRPC_HB;
     } else if(IS_LOW(height_1)) {
         this->type = WorkpieceType::WRPC_L;
     } else if(IS_HIGH(height_1)) {
