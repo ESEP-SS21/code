8223cb61732e8eefbf4222d44e2d50faff045129
Hugo Protsch <hugo.protsch@gmail.com>
2021-05-22 23:10:38 +0200

    Refactor tests: Make them OO by using text fixtures

--- esep_ss2021_Gruppe2_3/test/timer/AsyncTimerServiceTest.cpp
+++ esep_ss2021_Gruppe2_3/test/timer/AsyncTimerServiceTest.cpp
@@ -8,6 +8,8 @@
 #include <chrono>
 #include <string>
 
+using namespace dispatcher;
+
 constexpr int timerToleranceMs = 10;
 const std::string dispatcherPrefix = "TEST_DISP";
 const std::string clienetPrefix = "TEST_CLIENT";
@@ -17,70 +19,68 @@ std::string getUniqueDispatcherName(std::string name) {
     return fmt::format("{}_{}_{}", dispatcherPrefix, i++, name);
 }
 
-void testSingleTimerEventCode(int ms_time, int payload) {
-    const std::string DISPATCHER_NAME = getUniqueDispatcherName(__FUNCTION__);
-    dispatcher::Dispatcher disp(DISPATCHER_NAME);
-    timer::AsyncTimerService timer_svc(DISPATCHER_NAME);
-    TestClient client(DISPATCHER_NAME, fmt::format("{}_{}", clienetPrefix, __FUNCTION__));
-    client.subscribe(dispatcher::EventType::EVNT_TIM_ALRT);
+class TimerTest: public ::testing::Test {
+protected:
+    std::unique_ptr<Dispatcher> _dispatcher;
+    std::unique_ptr<timer::AsyncTimerService> _timerService;
+    std::unique_ptr<TestClient> _testClient;
+    TimerTest() {
+        const std::string DISPATCHER_NAME = getUniqueDispatcherName(__FUNCTION__);
+        _dispatcher = std::unique_ptr<Dispatcher>(new Dispatcher(DISPATCHER_NAME));
+        _timerService = std::unique_ptr<timer::AsyncTimerService>(
+                new timer::AsyncTimerService(DISPATCHER_NAME));
+        _testClient = std::unique_ptr<TestClient>(
+                new TestClient(DISPATCHER_NAME, fmt::format("{}_{}", clienetPrefix, __FUNCTION__)));
+        _testClient->subscribe(EventType::EVNT_TIM_ALRT);
+    }
 
-    dispatcher::Event e1 = { dispatcher::EventType::EVNT_TIM_REQ,
-            dispatcher::Event::generate_timer_payload(payload, ms_time), false };
+    void SetUp() {
+        // code here will execute just before the test ensues
+    }
 
-    int msg_pio = 20;
-    client.send(e1, msg_pio);
-    dispatcher::Event rcv = client.recieve_event();
-    ASSERT_EQ(rcv.payload, payload);
+    void TearDown() {
+        // code here will be called just after the test completes
+        // ok to through exceptions from here if need be
+    }
+
+    void testSingleTimerEventCode(int ms_time, int payload) {
+
+        dispatcher::Event e1 = { dispatcher::EventType::EVNT_TIM_REQ, Event::generate_timer_payload(
+                payload, ms_time), false };
+
+        int msg_pio = 20;
+        _testClient->send(e1, msg_pio);
+        Event rcv = _testClient->recieve_event();
+        ASSERT_EQ(rcv.payload, payload);
+    }
+
+    void testSingleTimerTimePeriod(int ms_time, int payload) {
+        dispatcher::Event e1 = { dispatcher::EventType::EVNT_TIM_REQ,
+                dispatcher::Event::generate_timer_payload(42, ms_time), false };
+
+        auto time_start = std::chrono::high_resolution_clock::now();
+        this->_testClient->send(e1, 20);
+        this->_testClient->recieve_event();
+        auto time_end = std::chrono::high_resolution_clock::now();
+        auto actual_ms_time =
+                std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
+
+        ASSERT_TRUE(actual_ms_time < ms_time + timerToleranceMs && actual_ms_time >= ms_time);
+    }
+};
+
+TEST_F(TimerTest, ReturnedEventShouldEqualExpectedEventCode) {
+    this->testSingleTimerEventCode(10, 44);
+    this->testSingleTimerEventCode(10, 45);
+    this->testSingleTimerEventCode(10, 44);
 }
 
-TEST(Timer, ReturnedEventCouldShouldEqualEventCodeWhichHasBeenSetUp) {
-    testSingleTimerEventCode(10, 44);
-    testSingleTimerEventCode(10, 45);
-    testSingleTimerEventCode(10, 44);
+TEST_F(TimerTest, TimePeriodShouldBeWithinTolerance) {
+    this->testSingleTimerTimePeriod(50, 44);
+    this->testSingleTimerTimePeriod(1500, 44);
 }
 
-TEST(Timer, TimePeriod) {
-    const std::string DISPATCHER_NAME = "TEST_DISP_TIMER_PERIOD";
-    dispatcher::Dispatcher disp(DISPATCHER_NAME);
-    timer::AsyncTimerService timer_svc(DISPATCHER_NAME);
-    TestClient client(DISPATCHER_NAME, "TEST_CLIENT_TIMER_PERIOD");
-    client.subscribe(dispatcher::EventType::EVNT_TIM_ALRT);
-
-    // short time period
-    int expectedTime = 100;
-    dispatcher::Event e1 = { dispatcher::EventType::EVNT_TIM_REQ,
-            dispatcher::Event::generate_timer_payload(42, expectedTime), false };
-
-    auto time_start = std::chrono::high_resolution_clock::now();
-    client.send(e1, 20);
-    client.recieve_event();
-    auto time_end = std::chrono::high_resolution_clock::now();
-    auto delta =
-            std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
-
-    ASSERT_TRUE(delta < expectedTime + timerToleranceMs && delta >= expectedTime);
-
-    // long time period
-    expectedTime = 1500;
-    dispatcher::Event e2 = { dispatcher::EventType::EVNT_TIM_REQ,
-            dispatcher::Event::generate_timer_payload(42, expectedTime), false };
-
-    time_start = std::chrono::high_resolution_clock::now();
-    client.send(e2, 20);
-    client.recieve_event();
-    time_end = std::chrono::high_resolution_clock::now();
-
-    delta = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
-    ASSERT_TRUE(delta < expectedTime + timerToleranceMs && delta >= expectedTime);
-}
-
-TEST(Timer, MultipleTimers) {
-    const std::string DISPATCHER_NAME = "TEST_DISP_TIMER_MULTIPLE";
-    dispatcher::Dispatcher disp(DISPATCHER_NAME);
-    timer::AsyncTimerService timer_svc(DISPATCHER_NAME);
-    TestClient client(DISPATCHER_NAME, "TEST_CLIENT_TIMER_MULTIPLE");
-    client.subscribe(dispatcher::EventType::EVNT_TIM_ALRT);
-
+TEST_F(TimerTest, MultipleTimers) {
     int payload1 = 42;
     int payload2 = 23;
     int expectedTimeShort = 100;
@@ -92,19 +92,19 @@ TEST(Timer, MultipleTimers) {
             dispatcher::Event::generate_timer_payload(payload2, expectedTimeLong), false };
 
     auto time_start = std::chrono::high_resolution_clock::now();
-    client.send(e1, 20);
-    client.send(e2, 20);
+    this->_testClient->send(e1, 20);
+    this->_testClient->send(e2, 20);
 
-    dispatcher::Event rcv1 = client.recieve_event();
+    dispatcher::Event rcv1 = this->_testClient->recieve_event();
     auto time_end = std::chrono::high_resolution_clock::now();
     ASSERT_EQ(rcv1.payload, payload1);
-    auto delta =
+    auto actual_ms_time =
             std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
-    ASSERT_TRUE(delta < expectedTimeShort + timerToleranceMs && delta >= expectedTimeShort);
+    ASSERT_TRUE(actual_ms_time < expectedTimeShort + timerToleranceMs && actual_ms_time >= expectedTimeShort);
 
-    dispatcher::Event rcv2 = client.recieve_event();
+    dispatcher::Event rcv2 = this->_testClient->recieve_event();
     time_end = std::chrono::high_resolution_clock::now();
     ASSERT_EQ(rcv2.payload, payload2);
-    delta = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
-    ASSERT_TRUE(delta < expectedTimeLong + timerToleranceMs && delta >= expectedTimeLong);
+    actual_ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
+    ASSERT_TRUE(actual_ms_time < expectedTimeLong + timerToleranceMs && actual_ms_time >= expectedTimeLong);
 }
