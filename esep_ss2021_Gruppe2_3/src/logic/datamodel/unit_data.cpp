3ef2bb49f7225855cc0c3c89e72102f766689c90
Justin Hoffmann <justin.hoffmann@haw-hamburg.de>
2021-05-22 21:26:17 +0200

    Implement wrpc_order_step and wrpc_order_reset

--- esep_ss2021_Gruppe2_3/src/logic/datamodel/unit_data.cpp
+++ esep_ss2021_Gruppe2_3/src/logic/datamodel/unit_data.cpp
@@ -17,11 +17,15 @@ bool UnitData::wrpc_fits_order(const Workpiece& wrpc) const {
 
 void UnitData::wrpc_order_step() {
     const std::lock_guard<std::mutex> lock(_unit_mutex);
-
+    _next_in_order =
+            (_next_in_order == WorkpieceType::WRPC_HM) ? WorkpieceType::WRPC_HB :
+            (_next_in_order == WorkpieceType::WRPC_HB) ? WorkpieceType::WRPC_L :
+            WorkpieceType::WRPC_HM;
 }
 
 void UnitData::wrpc_order_reset(WorkpieceType wrpc_type) {
     const std::lock_guard<std::mutex> lock(_unit_mutex);
+    _next_in_order = wrpc_type;
 }
 
 std::shared_ptr<CBSection> UnitData::get_switch_end_sec() const {
