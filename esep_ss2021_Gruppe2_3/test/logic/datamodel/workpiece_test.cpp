3f85f80f6d6b4ac2441265fd1c28e0a075be24a4
Hugo Protsch <hugo.protsch@gmail.com>
2021-05-23 18:04:47 +0200

    Add test 'FlippedShouldBeDeterminedCorrectly'

--- esep_ss2021_Gruppe2_3/test/logic/datamodel/workpiece_test.cpp
+++ esep_ss2021_Gruppe2_3/test/logic/datamodel/workpiece_test.cpp
@@ -10,8 +10,9 @@ namespace datamodel {
 using namespace ::logic::datamodel;
 
 template<int size>
-void testWorkpieceTypes(std::array<Workpiece, size>& workpieces, std::array<WorkpieceType, size>& workpieces_types){
-    for (unsigned int i = 0; i < workpieces.size(); i++){
+void testWorkpieceTypes(std::array<Workpiece, size>& workpieces,
+        std::array<WorkpieceType, size>& workpieces_types) {
+    for (unsigned int i = 0; i < workpieces.size(); i++) {
         workpieces[i].determine_type();
         ASSERT_EQ(workpieces[i].get_type(), workpieces_types[i]);
     }
@@ -30,7 +31,25 @@ TEST(WorkpieceTest, DeterminedTypeShouldBeCorrct) {
     std::array<WorkpieceType, size> workpieces_types { WorkpieceType::WRPC_L, WorkpieceType::WRPC_H,
             WorkpieceType::WRPC_HM, WorkpieceType::WRPC_HB, };
 
-   testWorkpieceTypes<size>(workpieces, workpieces_types);
+    testWorkpieceTypes<size>(workpieces, workpieces_types);
+}
+
+TEST(WorkpieceTest, FlippedShouldBeDeterminedCorrectly) {
+    Workpiece wp_hm, wp_hb;
+    wp_hb.height_1 = wp_hm.height_1 = Workpiece::height_bohrung;
+    wp_hm.is_metallic = true;
+
+    wp_hb.determine_type();
+    wp_hm.determine_type();
+    ASSERT_FALSE(wp_hb.is_flipped);
+    ASSERT_FALSE(wp_hm.is_flipped);
+
+    wp_hb.height_2 = wp_hm.height_2 = Workpiece::height_high;
+
+    wp_hb.determine_type();
+    wp_hm.determine_type();
+    ASSERT_TRUE(wp_hb.is_flipped);
+    ASSERT_TRUE(wp_hm.is_flipped);
 }
 
 } /*namespace*/
