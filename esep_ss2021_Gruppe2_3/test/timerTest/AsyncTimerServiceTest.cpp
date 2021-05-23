e75b6daefaa15bee8fd7ac105d1960680f19f3a6
Hugo Protsch <hugo.protsch@gmail.com>
2021-05-23 12:39:15 +0200

    Extract testMultipleTimerTimePeriod(), struct TimerData

--- esep_ss2021_Gruppe2_3/test/timerTest/AsyncTimerServiceTest.cpp
+++ esep_ss2021_Gruppe2_3/test/timerTest/AsyncTimerServiceTest.cpp
@@ -1,10 +1,7 @@
-#include <timerTest/TestClient.h>
+#include "TestClient.h"
 #include "tests.h"
-#include "../src/dispatcher/dispatcher.h"
-#include "../src/timer/ASyncTimerService.h"
-#include "../src/dispatcher/cnnMngmnt/QnxChannel.h"
-#include "../src/dispatcher/cnnMngmnt/QnxConnection.h"
 #include "../src/dispatcher/Event.h"
+
 #include <chrono>
 #include <string>
 
@@ -24,6 +21,15 @@ std::string getUniqueDispatcherName() {
     return fmt::format("{}_{}_{}", dispatcherPrefix, i++, __TC_NAME__);
 }
 
+bool timeIsWithinTolerance(long expected, long actual) {
+    return (actual < expected + timerToleranceMs && actual >= expected);
+}
+
+struct TimerData {
+    int ms_time;
+    int payload;
+};
+
 class TimerTest: public ::testing::Test {
 protected:
     std::unique_ptr<Dispatcher> _dispatcher;
@@ -36,33 +42,22 @@ protected:
                 new timer::AsyncTimerService(DISPATCHER_NAME));
         _testClient = std::unique_ptr<TestClient>(
                 new TestClient(DISPATCHER_NAME, fmt::format("{}_{}", clienetPrefix, __TC_NAME__)));
-        _testClient->subscribe({
-            dispatcher::EventType::EVNT_CTRL_T_STR_PRS_SRT,
-            dispatcher::EventType::EVNT_TIM_ALRT
-        });
+        _testClient->subscribe( { dispatcher::EventType::EVNT_CTRL_T_STR_PRS_SRT,
+                dispatcher::EventType::EVNT_TIM_ALRT });
     }
 
-    void SetUp() {
-        // code here will execute just before the test ensues
-    }
+    void testSingleTimerEventCode(TimerData timer) {
 
-    void TearDown() {
-        // code here will be called just after the test completes
-        // ok to through exceptions from here if need be
-    }
-
-    void testSingleTimerEventCode(int ms_time, int payload) {
-
-        dispatcher::Event e1 = Event::CreateTimer(payload, ms_time);
+        dispatcher::Event e1 = Event::CreateTimer(timer.payload, timer.ms_time);
 
         int msg_pio = 20;
         _testClient->send(e1, msg_pio);
         Event rcv = _testClient->recieve_event();
-        ASSERT_EQ(rcv.payload, payload);
+        ASSERT_EQ(rcv.payload, timer.payload);
     }
 
-    void testSingleTimerTimePeriod(int ms_time, int payload) {
-        dispatcher::Event e1 = Event::CreateTimer(42, ms_time);
+    void testSingleTimerTimePeriod(TimerData timer) {
+        dispatcher::Event e1 = Event::CreateTimer(timer.payload, timer.ms_time);
 
         auto time_start = std::chrono::high_resolution_clock::now();
         this->_testClient->send(e1, 20);
@@ -71,51 +66,46 @@ protected:
         auto actual_ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(
                 time_end - time_start).count();
 
-        ASSERT_TRUE(actual_ms_time < ms_time + timerToleranceMs && actual_ms_time >= ms_time);
+        timeIsWithinTolerance(timer.ms_time, actual_ms_time);
+    }
+
+    void testMultipleTimerTimePeriod(TimerData timer1, TimerData timer2) {
+        dispatcher::Event e1 = Event::CreateTimer(timer1.payload, timer1.ms_time);
+        dispatcher::Event e2 = Event::CreateTimer(timer2.payload, timer2.ms_time);
+
+        auto time_start = std::chrono::high_resolution_clock::now();
+        this->_testClient->send(e1, 20);
+        this->_testClient->send(e2, 20);
+
+        dispatcher::Event rcv1 = this->_testClient->recieve_event();
+        auto time_end = std::chrono::high_resolution_clock::now();
+        ASSERT_EQ(rcv1.payload, timer1.payload);
+        auto actual_ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+                time_end - time_start).count();
+        timeIsWithinTolerance(timer1.ms_time, actual_ms_time);
+
+        dispatcher::Event rcv2 = this->_testClient->recieve_event();
+        time_end = std::chrono::high_resolution_clock::now();
+        ASSERT_EQ(rcv2.payload, timer2.payload);
+        actual_ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+                time_end - time_start).count();
+        timeIsWithinTolerance(timer2.ms_time, actual_ms_time);
     }
 };
 
 TEST_F(TimerTest, ReturnedEventShouldEqualExpectedEventCode) {
-    this->testSingleTimerEventCode(10, 44);
-    this->testSingleTimerEventCode(10, 45);
-    this->testSingleTimerEventCode(10, 44);
+    this->testSingleTimerEventCode( { 10, 44 });
+    this->testSingleTimerEventCode( { 10, 45 });
+    this->testSingleTimerEventCode( { 10, 44 });
 }
 
 TEST_F(TimerTest, TimePeriodShouldBeWithinTolerance) {
-    this->testSingleTimerTimePeriod(50, 44);
-    this->testSingleTimerTimePeriod(1500, 44);
+    this->testSingleTimerTimePeriod( { 50, 44 });
+    this->testSingleTimerTimePeriod( { 1500, 44 });
 }
 
-bool timeIsWithinTolerance(long expected, long actual) {
-    return (actual < expected + timerToleranceMs && actual >= expected);
-}
-
-TEST_F(TimerTest, MultipleTimers) {
-    int payload1 = 42;
-    int payload2 = 23;
-    int expectedTimeShort = 100;
-    int expectedTimeLong = 200;
-
-    dispatcher::Event e1 = Event::CreateTimer(payload1, expectedTimeShort);
-    dispatcher::Event e2 = Event::CreateTimer(payload2, expectedTimeLong);
-
-    auto time_start = std::chrono::high_resolution_clock::now();
-    this->_testClient->send(e1, 20);
-    this->_testClient->send(e2, 20);
-
-    dispatcher::Event rcv1 = this->_testClient->recieve_event();
-    auto time_end = std::chrono::high_resolution_clock::now();
-    ASSERT_EQ(rcv1.payload, payload1);
-    auto actual_ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(
-            time_end - time_start).count();
-    timeIsWithinTolerance(expectedTimeShort, actual_ms_time);
-
-    dispatcher::Event rcv2 = this->_testClient->recieve_event();
-    time_end = std::chrono::high_resolution_clock::now();
-    ASSERT_EQ(rcv2.payload, payload2);
-    actual_ms_time =
-            std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
-    timeIsWithinTolerance(expectedTimeLong, actual_ms_time);
+TEST_F(TimerTest, TimePeriodShouldBeWithinToleranceWithMultipleTimers) {
+    this->testMultipleTimerTimePeriod( { 100, 42 }, { 200, 43 });
 }
 
 } /*namespace*/
