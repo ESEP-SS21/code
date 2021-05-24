939d65bee3a7a6b225c40a700193859045df1ffb
Hugo Protsch <hugo.protsch@gmail.com>
2021-05-24 12:32:09 +0200

    Fix workpiece order

--- esep_ss2021_Gruppe2_3/src/logic/datamodel/workpiece_type.h
+++ esep_ss2021_Gruppe2_3/src/logic/datamodel/workpiece_type.h
@@ -3,9 +3,14 @@
 namespace logic {
 namespace datamodel {
 
+/*
+ * All types above 'WRPC_H' are ordered by the desired sorting order.
+ * All types including and below 'WRPC_H' are to be discarded.
+ */
 enum class WorkpieceType {
-    WRPC_HM = 0, WRPC_HB, WRPC_H,
-    WRPC_L, //needs to be first, which is not in sorting order
+    WRPC_HM = 0, WRPC_HB, WRPC_L,
+    //^^^ need to be in sorting order ^^^
+    WRPC_H, // WRPC_H needs to be first in enum, which is to be discarded
     Unknown
 };
 
