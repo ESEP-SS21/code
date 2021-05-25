06d01f4dc22e102ba648d3ed7d4b9aa27103b8bc
Justin Hoffmann <justin.hoffmann@haw-hamburg.de>
2021-05-25 15:18:15 +0200

    Add testcase for cbsection creation

--- esep_ss2021_Gruppe2_3/test/logic/datamodel/cb_section_test.cpp
+++ esep_ss2021_Gruppe2_3/test/logic/datamodel/cb_section_test.cpp
@@ -10,10 +10,15 @@ namespace datamodel {
 using namespace ::logic::datamodel;
 
 TEST(CBSectionTest, CBSectionCreationShouldLinkMultipleSectionsCorrectly) {
-    std::shared_ptr<CBSection> p_sec_2 = std::make_shared<CBSection>();
+    std::shared_ptr<CBSection> p_sec_3 = std::make_shared<CBSection>();
+    std::shared_ptr<CBSection> p_sec_2 = std::make_shared<CBSection>(p_sec_3);
     std::shared_ptr<CBSection> p_sec_1 = std::make_shared<CBSection>(p_sec_2);
-    ASSERT_EQ(p_sec_2->get_queue(), nullptr);
-
+    ASSERT_EQ(p_sec_3->get_next_section(), nullptr);
+    ASSERT_EQ(p_sec_2->get_next_section(), p_sec_3);
+    ASSERT_EQ(p_sec_1->get_next_section(), p_sec_2);
+    ASSERT_EQ(p_sec_3->workpiece_count(), 0);
+    ASSERT_EQ(p_sec_2->workpiece_count(), 0);
+    ASSERT_EQ(p_sec_1->workpiece_count(), 0);
 }
 
 TEST(CBSectionTest, WorkpieceCountShouldCountWorkpiecesCorrectly) {
