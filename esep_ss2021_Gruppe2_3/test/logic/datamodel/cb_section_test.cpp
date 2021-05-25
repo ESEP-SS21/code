f8ebe1e282af8b43a7bd483d4c0e8ae0bd547ab5
Justin Hoffmann <justin.hoffmann@haw-hamburg.de>
2021-05-25 15:06:35 +0200

    Add test cases for CBsection

--- esep_ss2021_Gruppe2_3/test/logic/datamodel/cb_section_test.cpp
+++ esep_ss2021_Gruppe2_3/test/logic/datamodel/cb_section_test.cpp
@@ -1,10 +1,38 @@
 #include <gtest/gtest.h>
+#include <memory>
+
+#include "../../logic/datamodel/cb_section.h"
 
 namespace test {
 namespace logic {
 namespace datamodel {
 
-TEST(CbSectionTest, descriptiveName) {
+
+
+TEST(CBSectionTest, CBSectionCreationShouldLinkMultipleSectionsCorrectly) {
+    //std::shared_ptr<CBSection> p_sec_2 = make_shared<CBSection>();
+    //std::shared_ptr<CBSection> p_sec_1 = make_shared<CBSection>(p_sec_2);
+
+
+}
+
+TEST(CBSectionTest, WorkpieceCountShouldCountWorkpiecesCorrectly) {
+    //TODO
+}
+
+TEST(CBSectionTest, EnteringAndExitingWorkpiecesShouldManifestInWorkpieceCount) {
+    //TODO
+}
+
+TEST(CBSectionTest, FirstAndLastWorpieceShouldReturnCorrespondingWorkpieces) {
+    //TODO
+}
+
+TEST(CBSectoinTest, TransferWorkpieceShouldManifestInQueueStructure) {
+    //TODO
+}
+
+TEST(CBSectionTest, CBSectionCreation) {
     //TODO
 }
 
