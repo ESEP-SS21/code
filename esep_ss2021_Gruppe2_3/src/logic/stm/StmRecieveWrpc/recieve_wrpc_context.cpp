fbd837bb5fa1c932335e5f315c0cb309b00fadaf
Hugo Protsch <hugo.protsch@gmail.com>
2021-06-03 14:56:05 +0200

    Add Event -> method mapping

--- esep_ss2021_Gruppe2_3/src/logic/stm/StmRecieveWrpc/recieve_wrpc_context.cpp
+++ esep_ss2021_Gruppe2_3/src/logic/stm/StmRecieveWrpc/recieve_wrpc_context.cpp
@@ -6,9 +6,48 @@ namespace logic {
 namespace stm {
 namespace recieveWrpcStm {
 
-CONTEXT_CTOR(RecieveWrpcContext,Idle)
+CONTEXT_CTOR(RecieveWrpcContext, Idle)
+
+using namespace dispatcher;
 
 void RecieveWrpcContext::handle(Event e) {
+    auto *state = (RecieveWrpcBaseState *) (_state);
+
+    switch (e.type) {
+        case EventType::EVNT_SEN_LB_ST_BLCK:
+            state->lb_st_blck();
+            return;
+        case EventType::EVNT_ACT_BELT_STP:
+            state->belt_stp();
+            return;
+        case EventType::EVNT_ACT_BELT_FWD:
+            state->belt_fwd();
+            return;
+        case EventType::EVNT_CTRL_T_STR_PRS_SRT:
+            state->str_prs_srt();
+            return;
+        case EventType::EVNT_CTRL_T_STR_PRS_LNG:
+            state->str_prs_lng();
+            return;
+        case EventType::EVNT_CTRL_T_STP_PRS_SRT:
+            state->stp_prs_srt();
+            return;
+        case EventType::EVNT_CTRL_T_RST_PRS_SRT:
+            state->rst_prs_srt();
+            return;
+        case EventType::EVNT_SEN_ESTOP_ON:
+            state->estop_on();
+            return;
+        case EventType::EVNT_ERR:
+            state->err();
+            return;
+        case EventType::EVNT_ERR_GONE:
+            state->all_err_gone();
+            return;
+        default:
+            state->srv_done();
+            return;
+    }
 
 }
 
