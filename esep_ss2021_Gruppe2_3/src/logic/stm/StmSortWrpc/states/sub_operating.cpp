2654e32893f8ddceeb77f1365635bc2f612e1148
Justin Hoffmann <justin.hoffmann@haw-hamburg.de>
2021-06-07 13:16:28 +0200

    Make logic schlanker

--- esep_ss2021_Gruppe2_3/src/logic/stm/StmSortWrpc/states/sub_operating.cpp
+++ esep_ss2021_Gruppe2_3/src/logic/stm/StmSortWrpc/states/sub_operating.cpp
@@ -7,59 +7,60 @@ namespace logic {
 namespace stm {
 namespace sortWrpcStm {
 
-INIT_OPERATING_SUB_STM(SubOperating,WaitingForWrpc)
+INIT_OPERATING_SUB_STM(SubOperating, WaitingForWrpc)
 
-bool SubOperating::lb_sw_blck(){
+bool SubOperating::lb_sw_blck() {
     bool handled = _operating_substate->lb_sw_blck();
     // WaitingForWrpc -> NoDiscard / Discard
-    if(!handled && _operating_substate->has_super_exit_with_lb_sw_blck_from_waiting_for_wrpc()){
-        Workpiece unchecked_wrpc = _datamodel->get_height_switch_sec()->first_workpiece();
-        if(_datamodel->wrpc_fits_order(unchecked_wrpc)) { // in sorting order
-            _operating_substate->exit();
-            exit();
-            new(this) SubWfstc;
-            entry();
-            entry_wfstc();
-        } else { // not in sorting order
-            _operating_substate->exit();
-            exit();
-            new(this) SubWfstc;
-            entry();
-            entry_discard();
+    if (!handled) {
+        if (_operating_substate->has_super_exit_with_lb_sw_blck_from_waiting_for_wrpc()) {
+            Workpiece unchecked_wrpc = _datamodel->get_height_switch_sec()->first_workpiece();
+            if (_datamodel->wrpc_fits_order(unchecked_wrpc)) { // in sorting order
+                _operating_substate->exit();
+                exit();
+                new (this) SubWfstc;
+                entry();
+                entry_wfstc();
+            } else { // not in sorting order
+                _operating_substate->exit();
+                exit();
+                new (this) SubWfstc;
+                entry();
+                entry_discard();
+            }
         }
-    }
-    // RampFull -> NoDiscard
-    if(!handled && _operating_substate->has_super_exit_with_lb_sw_blck_from_ramp_full()) {
-        if(_datamodel->_unit_type == UnitType::PRIMARY) {
-            _operating_substate->exit();
-            exit();
-            new(this) SubWfstc;
-            entry();
-            entry_wfstc();
+        // RampFull -> NoDiscard
+        if (_operating_substate->has_super_exit_with_lb_sw_blck_from_ramp_full()) {
+            if (_datamodel->_unit_type == UnitType::PRIMARY) {
+                _operating_substate->exit();
+                exit();
+                new (this) SubWfstc;
+                entry();
+                entry_wfstc();
+            }
         }
     }
     return handled;
 }
 
-bool SubOperating::lb_ra_clr(){
+bool SubOperating::lb_ra_clr() {
     bool handled = _operating_substate->lb_ra_clr();
     return handled;
 }
 
-bool SubOperating::tim_alrt(int tim_id){
+bool SubOperating::tim_alrt(int tim_id) {
     bool handled = _operating_substate->tim_alrt(tim_id);
     return handled;
 }
 
-void SubOperating::entry_history(){
+void SubOperating::entry_history() {
     _operating_substate->entry();
 }
 
-void SubOperating::entry_waiting_for_ramp_to_clear(){
-    new(_operating_substate) WaitingForRampToClear;
+void SubOperating::entry_waiting_for_ramp_to_clear() {
+    new (_operating_substate) WaitingForRampToClear;
     _operating_substate->entry();
 }
-
 
 } /* namespace recieveWrpcStm */
 } /* namespace stm */
