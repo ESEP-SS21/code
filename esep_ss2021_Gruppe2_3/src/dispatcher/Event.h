05b35cbbcc51c27c8628fe3f9541026a122d6fc0
Justin Hoffmann <justin.hoffmann@haw-hamburg.de>
2021-06-08 10:21:43 +0200

    Rm eventtype prefix

--- esep_ss2021_Gruppe2_3/src/dispatcher/Event.h
+++ esep_ss2021_Gruppe2_3/src/dispatcher/Event.h
@@ -9,8 +9,6 @@
 
 namespace dispatcher {
 
-#define EVNT_PREFIX "EVNT_"
-
 enum class EventType { //make sure to add a string representation for each value
     EVNT_ERR = 0,
     EVNT_ERR_GONE,
@@ -73,64 +71,26 @@ enum class TimerID
 };
 
 inline std::ostream& operator<<(std::ostream& out, const EventType& e) {
-    static const std::string strs[static_cast<int>(EventType::SIZE) + 1] = {
-            "EVNT_ERR",
-            "EVNT_ERR_GONE",
-            "EVNT_WRN",
-            "EVNT_WRN_GONE",
-            "EVNT_HRTB",
-            "EVNT_ACK",
-            "EVNT_SEN_LB_ST_BLCK",
-            "EVNT_SEN_LB_ST_CLR",
-            "EVNT_SEN_LB_SW_BLCK",
-            "EVNT_SEN_LB_SW_CLR",
-            "EVNT_SEN_LB_RA_BLCK",
-            "EVNT_SEN_LB_RA_CLR",
-            "EVNT_SEN_LB_EN_BLCK",
-            "EVNT_SEN_LB_EN_CLR",
-            "EVNT_SEN_LB_HE_BLCK",
-            "EVNT_SEN_LB_HE_CLR",
-            "EVNT_SEN_METAL_DTC",
-            "EVNT_SEN_HEIGHT_REQ",
-            "EVNT_SEN_HEIGHT_HE",
-            "EVNT_SEN_ESTOP_ON",
-            "EVNT_SEN_ESTOP_OFF",
-            "EVNT_CTRL_T_STR_PRS_LNG",
-            "EVNT_CTRL_T_STR_PRS_SRT",
-            "EVNT_CTRL_T_STP_PRS_LNG",
-            "EVNT_CTRL_T_STP_PRS_SRT",
-            "EVNT_CTRL_T_RST_PRS_LNG",
-            "EVNT_CTRL_T_RST_PRS_SRT",
-            "EVNT_ACT_CTRL_T_STR_LED_ON",
-            "EVNT_ACT_CTRL_T_STR_LED_OFF",
-            "EVNT_ACT_CTRL_T_RST_LED_ON",
-            "EVNT_ACT_CTRL_T_RST_LED_OFF",
-            "EVNT_ACT_BELT_BWD",
-            "EVNT_ACT_BELT_FWD",
-            "EVNT_ACT_BELT_STP",
-            "EVNT_ACT_SORT_DSC",
-            "EVNT_ACT_SORT_NO_DSC",
-            "EVNT_ACT_SORT_RST",
-            "EVNT_ACT_STPL_LED_ON",
-            "EVNT_ACT_STPL_LED_OFF",
-            "EVNT_ACT_STPL_LED_BLNK_FST",
-            "EVNT_ACT_STPL_LED_BLNK_SLW",
-            "EVNT_WRPC_TRNS_RQ",
-            "EVNT_WRPC_FLP",
-            "EVNT_MOD_IDL",
-            "EVNT_MOD_OP",
-            "EVNT_MOD_ERR",
-            "EVNT_TIM_REQ",
-            "EVNT_TIM_ALRT",
-            "EVNT_CONN_LOST",
-            "EVNT_SRV_DONE",
-            "EVNT_HIST",
-            "EVNT_RST_TO_SRT",
+    static const std::string strs[static_cast<int>(EventType::SIZE) + 1] = { "EVNT_ERR",
+            "EVNT_ERR_GONE", "EVNT_WRN", "EVNT_WRN_GONE", "EVNT_HRTB", "EVNT_ACK",
+            "EVNT_SEN_LB_ST_BLCK", "EVNT_SEN_LB_ST_CLR", "EVNT_SEN_LB_SW_BLCK",
+            "EVNT_SEN_LB_SW_CLR", "EVNT_SEN_LB_RA_BLCK", "EVNT_SEN_LB_RA_CLR",
+            "EVNT_SEN_LB_EN_BLCK", "EVNT_SEN_LB_EN_CLR", "EVNT_SEN_LB_HE_BLCK",
+            "EVNT_SEN_LB_HE_CLR", "EVNT_SEN_METAL_DTC", "EVNT_SEN_HEIGHT_REQ", "EVNT_SEN_HEIGHT_HE",
+            "EVNT_SEN_ESTOP_ON", "EVNT_SEN_ESTOP_OFF", "EVNT_CTRL_T_STR_PRS_LNG",
+            "EVNT_CTRL_T_STR_PRS_SRT", "EVNT_CTRL_T_STP_PRS_LNG", "EVNT_CTRL_T_STP_PRS_SRT",
+            "EVNT_CTRL_T_RST_PRS_LNG", "EVNT_CTRL_T_RST_PRS_SRT", "EVNT_ACT_CTRL_T_STR_LED_ON",
+            "EVNT_ACT_CTRL_T_STR_LED_OFF", "EVNT_ACT_CTRL_T_RST_LED_ON",
+            "EVNT_ACT_CTRL_T_RST_LED_OFF", "EVNT_ACT_BELT_BWD", "EVNT_ACT_BELT_FWD",
+            "EVNT_ACT_BELT_STP", "EVNT_ACT_SORT_DSC", "EVNT_ACT_SORT_NO_DSC", "EVNT_ACT_SORT_RST",
+            "EVNT_ACT_STPL_LED_ON", "EVNT_ACT_STPL_LED_OFF", "EVNT_ACT_STPL_LED_BLNK_FST",
+            "EVNT_ACT_STPL_LED_BLNK_SLW", "EVNT_WRPC_TRNS_RQ", "EVNT_WRPC_FLP", "EVNT_MOD_IDL",
+            "EVNT_MOD_OP", "EVNT_MOD_ERR", "EVNT_TIM_REQ", "EVNT_TIM_ALRT", "EVNT_CONN_LOST",
+            "EVNT_SRV_DONE", "EVNT_HIST", "EVNT_RST_TO_SRT",
 
     };
-    return out << EVNT_PREFIX << strs[static_cast<int>(e)];
+    return out << strs[static_cast<int>(e)];
 }
-
 
 inline std::string str(EventType et) {
     std::ostringstream buffer;
@@ -142,7 +102,6 @@ struct Event {
     EventType type;
     int payload;
     bool broadcast;
-
 
     Event() = default;
 
@@ -167,20 +126,21 @@ struct Event {
         return buffer.str();
     }
 
-    bool operator== (const Event& e) const{
+    bool operator==(const Event& e) const {
         return e.type == type && e.payload == payload;
     }
 
-    bool operator!= (const Event& e) const{
+    bool operator!=(const Event& e) const {
         return !(*this == e);
     }
 
     static inline Event CreateTimer(TimerID id, uint16_t time_ms, bool broadcast = false) {
-        return Event { dispatcher::EventType::EVNT_TIM_REQ, (static_cast<uint16_t>(id) << 16) + time_ms, broadcast };
+        return Event { dispatcher::EventType::EVNT_TIM_REQ, (static_cast<uint16_t>(id) << 16)
+                + time_ms, broadcast };
     }
 
     static inline Event CreateError(dispatcher::EventType fix) {
-        return Event { dispatcher::EventType::EVNT_ERR, static_cast<uint16_t>(fix), true};
+        return Event { dispatcher::EventType::EVNT_ERR, static_cast<uint16_t>(fix), true };
     }
 
 private:
@@ -192,7 +152,6 @@ private:
         return evnt_id & (~0b01000000);
     }
 
-
 };
 
 struct EventSubscription {
@@ -203,7 +162,7 @@ struct EventSubscription {
 using nlohmann::json;
 
 inline void to_json(json& j, const Event& e) {
-    j = json{{"type", e.type}, {"payl", e.payload}, {"broad", e.broadcast}};
+    j = json { { "type", e.type }, { "payl", e.payload }, { "broad", e.broadcast } };
 }
 
 inline void from_json(const json& j, Event& e) {
