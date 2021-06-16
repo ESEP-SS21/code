6c640f0b767ddc26c05dbd71383745baeee060ba
Hugo Protsch <hugo.protsch@gmail.com>
2021-06-16 11:14:17 +0200

    Add sensors

--- esep_ss2021_Gruppe2_3/src/logic/stm/service_mode/service_mode_states.cpp
+++ esep_ss2021_Gruppe2_3/src/logic/stm/service_mode/service_mode_states.cpp
@@ -18,16 +18,38 @@ bool ServiceModeStm::ServiceModeState::handle(const Event &event) {
     }
 
     switch (_currStep) {
-        case Step::s1_lb_st:
+        case Step::calibrate_he_req:
+            _eventSender->send({EventType::EVNT_SEN_HEIGHT_REQ});
+            return true;
+        case Step::calibrate_he_he:
+            if (event.type == EventType::EVNT_SEN_HEIGHT_HE)
+                _eventSender->send({EventType::EVNT_SEN_HEIGHT_REQ, event.payload});
+            return true;
+        case Step::sen_lb_st:
             if (event.type == EventType::EVNT_SEN_LB_ST_CLR)
                 step_done();
+            return true;
+        case Step::sen_lb_en:
+            if (event.type == EventType::EVNT_SEN_LB_EN_CLR)
+                step_done();
+            return true;
+        case Step::sen_lb_ra:
+            if (event.type == EventType::EVNT_SEN_LB_RA_CLR)
+                step_done();
+            return true;
+        case Step::sen_lb_sw:
+            if (event.type == EventType::EVNT_SEN_LB_SW_CLR)
+                step_done();
+            return true;
+        case Step::sen_me:
+            if (event.type == EventType::EVNT_SEN_METAL_DTC)
+                step_done();
+            return true;
 
-
-        case Step::first:
-            break;
         case Step::DONE:
             _logger->error("");
     }
+
     return true;
 }
 
@@ -35,15 +57,14 @@ void ServiceModeState::handle_ack() {
     waiting_for_ack = false;
     _eventSender->send({EventType::EVNT_ACT_CTRL_T_RST_LED_OFF});
     _currStep = Step(static_cast<int>(_currStep) + 1);
-    CheckForEndOfService();
+    if (_currStep == Step::DONE)
+        end_service();
 }
 
-void ServiceModeState::CheckForEndOfService() {
-    if (_currStep == Step::DONE) {
-        _eventSender->send({EventType::EVNT_SRV_DONE, 0, true});
-        // clear state data
-        new(this) ServiceModeState();
-    }
+void ServiceModeState::end_service() {
+    _eventSender->send({EventType::EVNT_SRV_DONE, 0, true});
+    // clear state data
+    new(this) ServiceModeState();
 }
 
 void ServiceModeState::step_done() {
--- esep_ss2021_Gruppe2_3/src/logic/stm/service_mode/service_mode_states.h
+++ esep_ss2021_Gruppe2_3/src/logic/stm/service_mode/service_mode_states.h
@@ -1,29 +1,43 @@
 #pragma once
+
 #include "../base_base_state.h"
 
 namespace logic {
 namespace stm {
 namespace ServiceModeStm {
 
-enum class Step{
-    first,
-   s1_lb_st,
-   DONE
+enum class Step {
+    calibrate_he_req,
+    calibrate_he_he,
+    sen_lb_st,
+    sen_lb_en,
+    sen_lb_sw,
+    sen_lb_ra,
+    sen_me,
+    DONE,
 };
 
-class ServiceModeState: public BaseBaseState {
+class ServiceModeState : public BaseBaseState {
 private:
-    Step _currStep {Step::first};
+    Step _currStep;
     bool waiting_for_ack;
-    void CheckForEndOfService();
+
+    void end_service();
+
     void step_done();
 
 public:
     ServiceModeState() = default;
+
     STATE_HEADER_INIT
+
     bool handle(const Event &event) override;
 
     void handle_ack();
+
+    void exit() override{
+        end_service();
+    }
 };
 
 } /* namespace recieveWrpcStm */
